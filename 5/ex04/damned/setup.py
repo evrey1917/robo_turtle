459179a73c28542ca4b52d28cd2c9f19f7a3bd86
evrey1917 <143984979+evrey1917@users.noreply.github.com>
2024-11-12 00:33:47 +0700

    Add files via upload

---
+++ 5/ex04/damned/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+from glob import glob
+import os
+
+package_name = 'damned'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.py'))),
+        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*.urdf.xacro'))),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.rviz'))),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yaml'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='evrey',
+    maintainer_email='nikitaevreev1917@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'circle_movement = damned.circle_movement:main',
+        ],
+    },
+)

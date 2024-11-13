74a1dc1bab7452cf0ced307b9e1c6704beb70a54
evrey1917 <143984979+evrey1917@users.noreply.github.com>
2024-11-13 15:59:47 +0700

    Add files via upload

---
+++ 5/ex05/launch/circle_movement.launch.py
@@ -0,0 +1,35 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    demo_nodes = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('monk'), 'launch'),
+            '/diff_drive.launch.py']),
+       )
+
+    return LaunchDescription([
+        demo_nodes,
+        DeclareLaunchArgument(
+            'radius', default_value='1',
+            description='Radius.'
+        ),
+        Node(
+            package='damned',
+            executable='circle_movement',
+            name='circle_movement',
+            arguments=[
+                LaunchConfiguration('radius')
+            ]
+        ),
+    ])
